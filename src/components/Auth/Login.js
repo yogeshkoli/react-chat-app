a2093b455cc1c2d8a8538afcfde30d70970e739c
yogeshkoli <yogeshkoli@hotmail.com>
2020-01-31 17:41:49 +0530

    Updated login compoent to have basic features

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -4,23 +4,58 @@ import { Link } from 'react-router-dom';
 
 class Login extends Component {
 
+    state = {
+        email: '',
+        password: '',
+        errors: [],
+        loading: false,
+    }
+
+    handleChange = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
+    displayErrors = errors => errors.map((error, i) => <p key={i}>{error.message}</p>);
+
+    hanldeInputError = (errors, inputName) => {
+        return errors.some(error => error.message.toLowerCase().includes(inputName)) ? 'error' : '';
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        if (this.isFormValid()) {
+            this.setState({ errors: [], loading: true });
+
+        }
+    }
+
     render() {
+
+        const { email, password, errors, loading } = this.state;
+
         return (
             <Grid textAlign="center" verticalAlign="middle" className="app">
                 <Grid.Column style={{ maxWidth: 450 }}>
-                    <Header as="h2" icon color="purple" textAlign="center">
-                        <Icon name="puzzle piece" color="purple" />
-                        Login for React Chat
+                    <Header as="h1" icon color="violet" textAlign="center">
+                        <Icon name="code branch" color="violet" />
+                        Login to React Chat
                 </Header>
                     <Form size="large">
                         <Segment stacked>
-                            <Form.Input fluid name="username" icon="user" iconPosition="left" placeholder="Username" onChange={this.handeChange} type="text" />
+                            <Form.Input fluid name="email" icon="user" iconPosition="left" placeholder="Email" onChange={this.handeChange} type="text" className={this.hanldeInputError(errors, 'email')} />
 
-                            <Form.Input fluid name="password" icon="key" iconPosition="left" placeholder="Password" onChange={this.handeChange} type="password" />
+                            <Form.Input fluid name="password" icon="key" iconPosition="left" placeholder="Password" onChange={this.handeChange} type="password" className={this.hanldeInputError(errors, 'password')} />
 
-                            <Button color="purple" fluid size="large">Login</Button>
+                            <Button disabled={loading} className={loading ? 'loading' : ''} color="violet" fluid size="large">Login</Button>
 
-                            <Message>Do not have a account? <Link to="/register">Register</Link></Message>
+                            {errors.length > 0 && (
+                                <Message color="red" >
+                                    <Icon name="frown" />
+                                    {this.displayErrors(errors)}
+                                </Message>
+                            )}
+
+                            <Message>Don't have an account? <Link to="/register">Register</Link></Message>
                         </Segment>
                     </Form>
                 </Grid.Column>
