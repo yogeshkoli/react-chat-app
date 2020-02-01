4ffb853f136f4c2908c69c96a6d2c12e596620d4
yogeshkoli <yogeshkoli@hotmail.com>
2020-02-01 17:50:14 +0530

    listten user login auth state change to redirect uer on login or register

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './components/App';
@@ -6,21 +6,36 @@ import * as serviceWorker from './serviceWorker';
 
 import 'semantic-ui-css/semantic.min.css';
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 
-const Root = () => (
-    <Router>
-        <Switch>
-            <Route exact path="/" component={App} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/register" component={Register} />
-        </Switch>
-    </Router>
-);
+import firebase from './firebase';
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+class Root extends Component {
+
+    componentDidMount() {
+        firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                this.props.history.push('/');
+            }
+        });
+    }
+
+    render() {
+        return (
+            <Switch>
+                <Route exact path="/" component={App} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/register" component={Register} />
+            </Switch>
+        );
+    }
+}
+
+const RoutewithAuth = withRouter(Root);
+
+ReactDOM.render(<Router><RoutewithAuth /></Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
