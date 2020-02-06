bef17de7209d5227ac810ba1313c38af21b0f7c7
yogeshkoli <yogeshkoli@hotmail.com>
2020-02-06 10:44:49 +0530

    Added message listener to list all the messages from the current channel

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -3,26 +3,57 @@ import firebase from '../../firebase';
 import { Segment, Comment } from 'semantic-ui-react';
 import MessagesHeader from './MessagesHeader';
 import MessageForm from './MessageForm';
+import Message from './Message';
 
 class Messages extends Component {
 
     state = {
         messagesRef: firebase.database().ref('messages'),
+        messages: [],
+        messagesLoading: true,
         channel: this.props.currentChannel,
         user: this.props.currentUser
     }
 
+    componentDidMount() {
+        const { user, channel } = this.state;
+
+        if (user && channel) {
+            this.addListeners(channel.id);
+        }
+    }
+
+    addListeners = channelId => {
+        this.addMessageListener(channelId);
+    }
+
+    addMessageListener = channelId => {
+        let loadedMessages = [];
+        this.state.messagesRef.child(channelId).on('child_added', snap => {
+            loadedMessages.push(snap.val());
+            this.setState({ messages: loadedMessages, messagesLoading: false });
+        });
+    }
+
+    displayMessages = messages => (messages.length > 0 && messages.map(message => (
+        <Message
+            key={message.timestamp}
+            message={message}
+            user={this.state.user}
+        />
+    )));
+
     render() {
 
-        const { messagesRef, channel, user } = this.state;
+        const { messagesRef, channel, user, messages } = this.state;
 
         return (
             <React.Fragment>
-                <MessagesHeader />
+                <MessagesHeader currentChannel={channel} />
 
                 <Segment>
                     <Comment.Group className="messages">
-                        {/* Messages */}
+                        {this.displayMessages(messages)}
                     </Comment.Group>
                 </Segment>
 
