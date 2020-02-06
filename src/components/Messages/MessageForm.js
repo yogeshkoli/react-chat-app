dc1574654f96f3ca43c35ceb600fb4ccf9a0fabc
yogeshkoli <yogeshkoli@hotmail.com>
2020-02-06 13:57:43 +0530

    Showing progress bar to indicate image upload status

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -3,6 +3,7 @@ import { Segment, Button, Input } from 'semantic-ui-react';
 import firebase from '../../firebase';
 import uuidv4 from 'uuid/v4';
 import FileModal from './FileModal';
+import ProgressBar from './ProgressBar';
 
 class MessageForm extends Component {
 
@@ -136,7 +137,7 @@ class MessageForm extends Component {
 
     render() {
 
-        const { errors, message, loading, modal } = this.state;
+        const { errors, message, loading, modal, uploadState, percentageUploaded } = this.state;
 
         return (
             <Segment className="message__form">
@@ -170,13 +171,15 @@ class MessageForm extends Component {
                         labelPosition="right"
                         icon="cloud upload"
                     />
-
-                    <FileModal
-                        modal={modal}
-                        closeModal={this.closeModal}
-                        uploadFile={this.uploadFile}
-                    />
                 </Button.Group>
+
+                <FileModal
+                    modal={modal}
+                    closeModal={this.closeModal}
+                    uploadFile={this.uploadFile}
+                />
+
+                <ProgressBar uploadState={uploadState} percentageUploaded={percentageUploaded} />
             </Segment>
         );
     }
